1a9b5e1f15b971d8ac7b1d33182a3e79e01527e7
Louis Desplanche <l.desplanche@gmail.com>
2019-01-15 11:10:40 +0100

    Base

---
+++ Zuulbad/Test.java
@@ -0,0 +1,19 @@
+
+/**
+ * Décrivez votre classe Test ici.
+ *
+ * @author Louis DESPLANCHE
+ * @version 1.0
+ */
+public class Test
+{
+    public Test()
+    {
+        System.out.println("Coucou");
+    }
+    
+    public static void Bonjour()
+    {
+        System.out.println("Je suis une variable statique qui dit bonjour !");
+    }
+}
