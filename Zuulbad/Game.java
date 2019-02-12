66c8dbee1f18d4ea69d782594d64784f33a2df83
Louis Desplanche <l.desplanche@gmail.com>
2019-02-12 22:00:00 +0100

    7.18.4
    Changement du titre

--- Zuulbad/Game.java
+++ Zuulbad/Game.java
@@ -33,7 +33,7 @@ public class Game
      */
     private void printWelcome()
     {
-        System.out.println("Welcome to the World of Zuul!");
+        System.out.println("Bienvenue dans Antisophia");
         System.out.println("World of Zuul is a new, incredibly boring adventure game.");
         System.out.println("Type 'help' if you need help.");
         System.out.println(" ");
